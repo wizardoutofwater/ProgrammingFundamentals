d06edd3810776df3803892291649783a197e157f
Mike McDonald <mike.mcd@me.com>
2020-10-29 14:00:45 -0500

    working version of JS homework assignments. no fancy stuff yet.

--- IntroToJsHomework.js
+++ IntroToJsHomework.js
@@ -21,3 +21,46 @@ function madlib(name,subj){
 }
 madlib("Mike", "Art")
 
+//Tip Calculator
+
+function tipAmount (billAmt, servLevel){
+    if (servLevel === `good`){
+        console.log (billAmt*0.2);
+    }else if (servLevel === `fair`){
+        console.log (billAmt*0.15);
+    }else if (servLevel === `bad`){
+        console.log (billAmt*0.1);
+    }
+    
+}
+tipAmount(100, `good`)    
+
+//Tip Calculator Part 2 
+
+function totalAmount (billAmt, servLevel){
+    if (servLevel === `good`){
+        console.log(billAmt * 0.2 + billAmt)
+    }else if (servLevel === `fair`){
+        console.log(billAmt * 0.15 + billAmt)
+    }else if (servLevel === `bad`){
+        console.log(billAmt * 0.1 + billAmt)
+    }
+}
+
+totalAmount(100, `good`)
+totalAmount(40, `fair`)
+
+//Tip Calculator part 3
+
+function splitAmount (billAmt, servLevel, numGuests){
+    if (servLevel === `good`){
+        console.log((billAmt * 0.2 + billAmt) / numGuests);
+    }else if (servLevel === `fair`){
+        console.log((billAmt * 0.15 + billAmt) / numGuests);
+    }else if (servLevel === `bad`){
+        console.log((billAmt * 0.1 + billAmt) / numGuests);
+    }
+
+}
+splitAmount(100, `good`, 5)
+splitAmount(40, `fair`, 2)
