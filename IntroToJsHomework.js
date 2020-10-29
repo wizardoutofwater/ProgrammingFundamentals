115090d12e58a83b845abcb8369ac50ac96f2dd4
Mike McDonald <mike.mcd@me.com>
2020-10-28 19:29:54 -0500

    First attempts at Intro to JS Homework

---
+++ IntroToJsHomework.js
@@ -0,0 +1,23 @@
+// Intro to Js Homework 
+
+//Hello, You!
+function hello(name){
+    console.log("Hello, "+ name +"!")
+}
+
+hello(`Mustache`)
+
+//Hello, You! Part 2
+function hello(name=`world`){
+    console.log("Hello, "+ name +"!")
+}
+
+hello(`Mustache`)
+hello()
+
+//Madlib
+function madlib(name,subj){
+    console.log(name + "'s favorite subject in school is " + subj )
+}
+madlib("Mike", "Art")
+
